504bbc12b71ed30de9b2685b939553806d872446
Max <dmxn8a@gmail.com>
2020-06-09 23:15:48 +1200

    index

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <ul class="menu">
-        <li><a href="index.html">Home</a></li>
+        <li><a href="index.php">Home</a></li>
        <!--- <li><a href="page2-dogfood.html">Dog Food</a></li>
         <li><a href="page3-catfood.html">Cat Food</a></li>
         <li><a href="page4-birdfood.html">Bird Food</a></li>
