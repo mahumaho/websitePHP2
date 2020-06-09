27e1a23600f3bb0f514457a74fa64a4aeb148527
Max <dmxn8a@gmail.com>
2020-06-09 22:56:10 +1200

    hopefully

--- class/DB.php
+++ class/DB.php
@@ -4,7 +4,7 @@ class DB
 {
     private $server = "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     private $user = "rv8kk7ab6a9led76";
-    private $pwd = " co5p03pim1xq00me";
+    private $pwd = "co5p03pim1xq00me";
     private $database = "s3ykxtpz1s85tbul";
     public $connection;
 
--- header.php
+++ header.php
@@ -4,4 +4,15 @@
         <li><a href="page3-catfood.html">Cat Food</a></li>
         <li><a href="page4-birdfood.html">Bird Food</a></li>
         <li><a href="page5-fishfood.html">Fish Food</a></li>--->
+        <?php
+            include_once "class/User.php";
+            $user = new User(null, "", "", "");
+            $categories = $user->viewCategories();
+            $i = 0;
+            while ($i < sizeof($categories)){
+                $category = $categories[$i];
+                echo "<li><a href='products.php?categoryID=".$category->id."'>".$category->name."</a></li>";
+                $i = $i + 1;
+            }
+            ?>
     </ul>
